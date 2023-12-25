0a31d354a8a1224e836ac70794c810f4e0372d11
Soichiro Yoshimura <yoshimura@soichiro.org>
2023-12-25 14:06:53 +0900

    Multの実装まで

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,4 +7,23 @@
 // Assumes that R0 >= 0, R1 >= 0, and R0 * R1 < 32768.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-//// Replace this comment with your code.
+// R0 と R1 を掛け算して R2 に格納する。
+// これは、R1 を R0 回足し算することで実現できる。
+    @R2
+    M=0
+(LOOP)
+    @R0
+    D=M
+    @END
+    D;JEQ
+    @R1
+    D=M
+    @R2
+    M=M+D
+    @R0
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
