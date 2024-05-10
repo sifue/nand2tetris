4e3c821c9f704583273fa835af5edcbad366b6ab
Soichiro Yoshimura <yoshimura@soichiro.org>
2024-05-10 17:30:04 +0900

    一旦multの実装までしたが次回再度最初から説明

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,4 +7,30 @@
 // Assumes that R0 >= 0, R1 >= 0, and R0 * R1 < 32768.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-//// Replace this comment with your code.
+// RO * R1 を計算して R2 に格納する
+// 残念ながら、+と-しか使えないのでどうするか？
+// 実は、掛け算は足し算を繰り返すことで実現できる
+
+  // まずR2を0で初期化、これをしとかないと実はバグる
+  @R2
+  M=0 // R2 = 0
+(LOOP) // LOOPラベル
+  // R1が0なら、ENDに飛んで終了
+  @R1 
+  D=M // D=R1
+  @END // jumpラベルを設定
+  D;JEQ // R1==0なら、ENDに飛ぶ
+  // R2にR0を足す
+  @R0
+  D=M // D=R0
+  @R2
+  M=M+D // R2=R2+R0
+  // R1を1減らす
+  @R1
+  M=M-1 // R1=R1-1
+  // ループラベルに飛ぶ
+  @LOOP
+  0;JMP
+(END) // ここでEND後ループし続けるようにする
+  @END
+  0;JMP
